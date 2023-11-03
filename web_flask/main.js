54010ecc4ae8f50be227e9be180dcb9348c43a7b
Okello Sam <samuelokello929@gmail.com>
2023-11-03 14:47:31 +0300

    js

--- web_flask/main.js
+++ web_flask/main.js
@@ -1,9 +1,17 @@
-document.getElementById('pickup-btn').addEventListener('click', function() {
-    var pickupForm = document.getElementById('pickup-form');
-    if (pickupForm.style.display === 'none' || pickupForm.style.display === '') {
+// document.getElementById('pickup-btn').addEventListener('click', function() {
+//     var pickupForm = document.getElementById('pickup-form');
+//     if (pickupForm.style.display === 'none' || pickupForm.style.display === '') {
+//         pickupForm.style.display = 'block';
+//     } else {
+//         pickupForm.style.display = 'none';
+//     }
+// });
+
+
+let requestBtn = document.getElementById("pickup-btn");
+requestBtn.addEventListener('click', function(){
+    let pickupForm = document.getElementById("pickup-form");
+    if (pickupForm.style.display == "none"){
         pickupForm.style.display = 'block';
-    } else {
-        pickupForm.style.display = 'none';
     }
 });
-
